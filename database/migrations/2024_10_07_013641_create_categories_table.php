3530afd186973d12f5b00ef10063fb172f72443a
LordRaiden <zeyadmo810@gmail.com>
2024-10-07 06:09:01 +0300

    tailwind try

--- database/migrations/2024_10_07_013641_create_categories_table.php
+++ database/migrations/2024_10_07_013641_create_categories_table.php
@@ -11,8 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create(table: 'categories', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('parent_id')->constrained('categories')->onDelete('cascade');
             $table->timestamps();
         });
     }
