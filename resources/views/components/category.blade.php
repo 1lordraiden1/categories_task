72fe8dbd7079c3230823dbbee94a02b09c4c431f
LordRaiden <zeyadmo810@gmail.com>
2024-10-08 08:33:41 +0300

    js added but no view

--- resources/views/components/category.blade.php
+++ resources/views/components/category.blade.php
@@ -9,3 +9,45 @@
         </div>
     @endforeach
 </div>
+<script>
+    $(document).ready(function () {
+        $(".multiLevelDropdownButton").each(function (_, dropdown) {
+            const dropdownMenu = $(dropdown).find("> .multi-dropdown")[0];
+            let popperInstance = null;
+
+            function create() {
+                popperInstance = Popper.createPopper(dropdown, dropdownMenu, {
+                    placment: 'auto-start',
+                    strategy: 'absolute',
+                    modifires: [{
+                        name: "flip",
+                        options: {
+                            fallbackPlacements: ["top", "bottom", "left", "right"],
+                        }
+                    }]
+                });
+            }
+
+            function destroy() {
+                if (popperInstance) {
+                    popperInstance.destroy();
+                    popperInstance = null;
+                }
+            }
+
+            function show() {
+                $(dropdownMenu).attr('data-show', "");
+                create();
+            }
+
+            function hide() {
+                $(dropdownMenu).removeAttr('data-show');
+                destroy();
+            }
+
+            $(dropdown).on("mouseenter focus", show);
+            $(dropdown).on("mouseenter blur", hide);
+
+        });
+    });
+</script>
